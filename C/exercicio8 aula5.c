8b6ec8a9d44a9737a170fec2bd3cefaf5f2e97aa
Max <maxthomaz2006@gmail.com>
2025-08-20 19:02:22 -0300

    commitando exercicio 8 aula 5 e corrigindo numeracao de vetor

--- C/exercicio8 aula5.c
+++ C/exercicio8 aula5.c
@@ -1,5 +1,5 @@
 /*Faça um programa em C que lê um vetor de inteiros de 20 posições. A partir desse preenchimento, ordene esse
-vetor em ordem crescente (sem criar outro vetor). Em seguida, imprima esse vetor ordenado.*/
+vetor em ordem decrescente (sem criar outro vetor). Em seguida, imprima esse vetor ordenado.*/
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -15,18 +15,18 @@ int main()
     }
     do{
         swap = false;
-        for(i=1;i<20;i++){
-            if(x[i]<x[i-1]){
-                troca = x[i-1];
-                x[i-1]=x[i];
-                x[i]= troca;
+        for(i=0;i<20;i++){
+            if(x[i]>x[i-1]){
+                troca = x[i];
+                x[i]=x[i-1];
+                x[i-1]= troca;
                 swap = true;
             }
         }
     }
     while(swap == true);
 
-    printf("x em ordem crescente");
+    printf("x em ordem decrescente");
     for (i=0;i<20;i++){
         printf("\n%i\n", x[i]);
     }
