3d404dd85dde3035d4b4cf104cf94ee1c7d80707
Max Thomazini Barbosa <max.tb@puccampinas.edu.br>
2025-09-22 11:29:42 -0300

    commitando ate o exercicio 7 da aula9

--- C/aula 9/todos os exercicios aula9.c
+++ C/aula 9/todos os exercicios aula9.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 // exercicio 1
 int par(int a){
@@ -28,7 +29,6 @@ double fracio(double a){
 }
 
 //exercicio3
-
 int fat(int a){
     int soma=0;
     for (int i = 0; i<=a; i++){
@@ -36,7 +36,46 @@ int fat(int a){
     }
     return soma;
 }
+
+//exercicio4
+int num_pares (int a, int b){
+    int contador = 0;
+    for (int i=a; i<b; i++){
+        if(i%2 == 0){
+            contador += 1;
+        }
+    }
+    return contador;
+}
+
+//exercicio5
+int tam_str (char string[]){
+    return strlen(string);
+}
+
+//exercicio6
+int qnt_char (char string[], char a){
+    int qnt = 0;
+    for (int i =0; string[i] != '\0'; i++){
+        if(string[i] == a) {
+            qnt ++;
+        }
+    }
+    return qnt;
+}
+
+//exercicio7
+void cop_str(char origem[], char destino[]){
+    int tamanho = strlen(destino);
+    strncpy(destino, origem, sizeof(tamanho));
+    destino[tamanho-1] = '\0';
+}
 int main(){
-    int fatorial = fat(10);
-    printf("%i", fatorial);
+    char string_org[] ="oi tudo bem";
+    char string_destino[strlen(string_org)+1];
+    cop_str(string_org, string_destino);
+
+    printf("String de origem: %s\n", string_org);
+    printf("String de destino: %s\n", string_destino);
+    return 0;
 }
