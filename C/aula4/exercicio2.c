eae6e4247ecb2e3ff64aeb682f25305176832ce8
Max <maxthomaz2006@gmail.com>
2025-10-04 15:21:07 -0300

    commitando exercicio2 da aula4

---
+++ C/aula4/exercicio2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int num, menor =100000, maior = 0, qnt = 0;
+
+    do
+    {
+        printf("Digite um numero: ");
+        scanf("%i", &num);
+        if (num == 0){
+            break;
+        }
+        qnt += 1;
+
+        if(maior<num){
+            maior=num;
+        }
+
+        if(menor>num){
+            menor=num;
+        }
+
+    } while (num != 0);
+    
+    printf("Maior: %i Menor: %i Quantos numeros foram digitados: %i", maior, menor, qnt);
+    return 0;
+}
