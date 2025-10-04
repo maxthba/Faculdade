8e2dcd0c3c8c604790ce12e96919ba4bf50a95f3
Max <maxthomaz2006@gmail.com>
2025-10-04 13:10:21 -0300

    commitando exercicio1 da aula2

---
+++ C/aula2/exercicio1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int A = 5;
+    int B = 13;
+
+    int temp = 0;
+
+    temp = A;
+    A = B;
+    B = temp;
+
+    printf("A = %i \n B = %i", A, B );
+
+    return 0;
+}
+ 
