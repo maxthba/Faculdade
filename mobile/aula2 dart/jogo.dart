af34ffc591be89ad41314369dad8cabb56b7de24
Max Thomazini Barbosa <max.tb@puccampinas.edu.br>
2026-02-26 11:21:50 -0300

    commitando resto da aula

--- mobile/aula2 dart/jogo.dart
+++ mobile/aula2 dart/jogo.dart
@@ -11,7 +11,7 @@ abstract class Animal{
 class dog extends Animal{
   @override
   void show(){
-    super.show;
+    super.show();
     print('caminhando ate o centro... parou...');
   }
   void bark(){
@@ -22,10 +22,24 @@ class dog extends Animal{
 class cat extends Animal{
   @override
   void show(){
-    super.show;
-    print('pulando de arvore em arvore... parou...');
+    super.show();
+    print('pulando de arvore em arvore... parou no centro...');
   }
   void meow(){
     print('miauu...');
   }
+}
+
+void main(){
+  var c = cat();
+  c.show();
+  c.meow();
+  c.hide();
+  print('opções de palavras para a criança clicar');
+
+  var d = dog();
+  d.show();
+  d.bark();
+  d.hide();
+  print('opções de palavras para a criança clicar');
 }
