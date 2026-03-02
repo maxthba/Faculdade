29da6c68132db0ad664b79accdea3c43251baf1f
Max Thomazini Barbosa <max.tb@puccampinas.edu.br>
2026-03-02 09:22:34 -0300

    commitando complementos da aula 4 em relação a igualdade

--- mobile/aula4 conceitos/enum.dart
+++ mobile/aula4 conceitos/enum.dart
@@ -19,10 +19,32 @@ enum StatusLampada {
 
 class Lampada {
   int id;
+  String modelo;
   StatusLampada status;
   Lampada(this.id, this.status);
+
+  bool equals(Lampada outra){
+    if(outra.id == this.id){
+      return true;
+    }
+    return false;
+  }
 }
 
 void main() {
+  Lampada l1 = Lampada(1, 'A01', StatusLampada.offline);
+  Lampada l2 = Lampada(1, 'A01', StatusLampada.offline);
+  if(l1 == l2){
+    print('são as mesmas lampadas');
+  } else{
+    print('Não são as mesmas lampadas = instancias diferentes');
+  }
 
+  /*if(l1.id == l2.id){
+    print('para a impresa são as mesmas lampadas');
+  }*/
+
+  if(l1.equals(l2)){
+    print('sao as mesmas lampadas');
+  }
 }
