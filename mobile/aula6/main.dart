ec5dbca6782942cb390229d00ef66dc02e202f65
Max <maxthomaz2006@gmail.com>
2026-03-18 12:43:28 -0300

    tornando exemplos doque nao fazer em comentarios

--- mobile/aula6/main.dart
+++ mobile/aula6/main.dart
@@ -4,7 +4,7 @@ void main(){
     Pessoa p1 = Pessoa('Max', 60, 1.76);
     print(p1.imc);
     //nao da para alterar mais imc
-    p1.imc = 10;
-    p1._imc = 10;
+    // p1.imc = 10;
+    // p1._imc = 10;
     
 }
