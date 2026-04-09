57bd8a8b88acb97763da3cb62273b1d68d1f33b6
Max <maxthomaz2006@gmail.com>
2026-04-09 15:23:30 -0300

    commitando final de exercicio de tiresshop

--- mobile/tiresshop_backend/functions/src/index.ts
+++ mobile/tiresshop_backend/functions/src/index.ts
@@ -7,23 +7,44 @@ const db = app.firestore();
 const colPneus = db.collection("pneus");
 
 export const addSampleTire = onRequest(
-  {region: "southamerica-east1"},
+  {region: "southamerica-east1", invoker: "public"},
   async (_request, response) => {
     const pneu = {
       largura: 225,
       altura: 50,
       aro: 17,
-      marca: "prielli",
-      modelo: "cinturato P7",
+      marca: "Pirelli",
+      modelo: "Cinturato P7",
       precoVenda: 780.99,
     };
-
     try {
       const docRef = await colPneus.add(pneu);
-      response.send("pneu exemplo inserido. Referencia " + docRef.id);
+      response.send("Pneu exemplo inserido. Referencia: " + docRef.id);
     } catch (e) {
       logger.error("erro ao inserir o pneu de exemplo.", e);
       response.status(500).send("erro ao inserir o pneu de exemplo");
     }
+  },
+);
+
+export const deleteTire = onRequest(
+  {region: "southamerica-east1", invoker: "public"},
+  async (_request, response) => {
+    const tireId = "lWsCYLsttVBP13NzHkuM";
+    await colPneus.doc(tireId).delete();
+    response.send("Exclusao -  provavelmente - realizada");
+  },
+);
+
+export const showSampleTires15 = onRequest(
+  {region: "southamerica-east1", invoker: "public"},
+  async (_request, response) => {
+    // busca de pneus com aro 15.
+    const tires: FirebaseFirestore.DocumentData[] = [];
+    const snapshot = await colPneus.where("aro", "==", 15).get();
+    snapshot.forEach((doc) => {
+      tires.push(doc.data());
+    });
+    response.status(200).json(tires);
   },
 );
